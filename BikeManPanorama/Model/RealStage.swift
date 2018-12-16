38da1717e747bab311d10d5222425d9876defbc9
Kohei Arai <a_la_mode1108@icloud.com>
2018-12-16 10:16:11 +0900

    added real time movie

---
+++ BikeManPanorama/Model/RealStage.swift
@@ -0,0 +1,13 @@
+//
+//  realStage.swift
+//  BikeManPanorama
+//
+//  Created by Kohei Arai on 2018/12/16.
+//  Copyright © 2018年 Kohei Arai. All rights reserved.
+//
+
+import Foundation
+
+struct RealStage {
+    var img: [Dictionary<String, Int>]
+}
--- BikeManPanorama/Util/GameScene.swift
+++ BikeManPanorama/Util/GameScene.swift
@@ -19,10 +19,13 @@ final class GameScene: SKScene {
     private var play: SKSpriteNode?
     private var background: SKSpriteNode?
     private var line: SKShapeNode?
+    private var movieBG: SKSpriteNode?
+    private var countdownLabel: SKLabelNode?
     
     //parameters
     private var groundList = [(minX: CGFloat, minY: CGFloat, maxX: CGFloat, maxY: CGFloat)]()
     private var pointList = [CGPoint]()
+    private var realStageList = [RealStage]()
     private var isGameStarted = false
     private var isAnimating = false
     
@@ -41,40 +44,102 @@ final class GameScene: SKScene {
     private var lineXMin: CGFloat = 10000
     private var lineYMax: CGFloat = 0
     private var lineYMin: CGFloat = 10000
+    private var count: Int = 3
+    
+    private let bikeManCategory: UInt32 = 0x1 << 1
+    private let groundCategory: UInt32 = 0x1 << 2
     
     override func didMove(to view: SKView) {
 //        createBackground()
         setupNodesInGameScene()
         createPointListFromCSV()
-        createMovie()
+//        createMovie()
 //        createLine()
 //        createGroundListMock()
-        var count = 0
-        var shortPointList = [CGPoint]()
-        shortPointList = Array(pointList[0..<200])
-        if let line = line {
-            let path = CGMutablePath()
-            shortPointList = Array(pointList[0..<200])
-            path.addLines(between: shortPointList)
-            path.closeSubpath()
-            line.physicsBody = SKPhysicsBody(edgeChainFrom: path)
-            line.physicsBody?.affectedByGravity = false
-            line.physicsBody?.isDynamic = false
-            line.physicsBody?.pinned = true
-            line.lineWidth = 100
-            line.zPosition = 1
-            line.strokeColor = UIColor.red
-            self.addChild(line)
+        createMovieBG()
+        countdown(count: count)
+        
+    }
+    
+    private func getJSONData() throws -> Data? {
+        guard let path = Bundle.main.path(forResource: "VID_cropped.max", ofType: "json") else { return nil }
+        let url = URL(fileURLWithPath: path)
+        
+        return try Data(contentsOf: url)
+    }
+    
+    private func createRealTimeLine() {
+        
+        guard let data = try? getJSONData() else { return }
+        var currentFrameCount = 0
+        
+        do {
+            let jsonResponse = try JSONSerialization.jsonObject(with: data!, options: [])
+            guard let jsonArray = jsonResponse as? [[String: Any]] else {
+                return
+            }
+            realStageList = jsonArray.map { (json) -> RealStage in
+                let realStage = RealStage(img: json["img"] as! [Dictionary<String, Int>])
+                return realStage
+            }
+            
+            let cgPointList: [[CGPoint]] = realStageList.map({return $0.img.map({return CGPoint(x: CGFloat($0["x"] as! Int) * CGFloat(3.38) - CGFloat(self.size.width / 2), y: self.size.height - CGFloat($0["y"] as! Int) * CGFloat(3.38) - CGFloat(self.size.height / 2))})})
+            
+            var tempCgList: [CGPoint] = cgPointList[(cgPointList.count - currentFrameCount) % cgPointList.count]
+            
+            line = SKShapeNode(points: &tempCgList, count: tempCgList.count)
+            line?.physicsBody?.categoryBitMask = groundCategory
+            line?.physicsBody?.contactTestBitMask = bikeManCategory
+            line?.physicsBody?.collisionBitMask = bikeManCategory
+            //        let lineWidth = lineXMax - lineXMin
+            //        let lineHeight = lineYMax - lineYMin
+            //        let lineWidth: CGFloat = 1200
+            //        let lineHeight: CGFloat = 800
+            //        line.position = CGPoint(x: lineWidth / 2, y: lineHeight / 2)
+            //        let lineSize =
+            if let line = line {
+                let path = CGMutablePath()
+                path.addLines(between: tempCgList)
+                path.closeSubpath()
+                line.physicsBody = SKPhysicsBody(edgeChainFrom: path)
+                line.physicsBody?.affectedByGravity = false
+                line.physicsBody?.isDynamic = false
+                line.physicsBody?.pinned = true
+                line.zPosition = 1
+                line.strokeColor = UIColor.white
+                self.addChild(line)
+            }
+        } catch let parsingError {
+            print("Error", parsingError)
         }
+        
+        
         let movieTimer = Timer.scheduledTimer(withTimeInterval: 0.03, repeats: true, block: { (timer) in
+            currentFrameCount += 1
+            if currentFrameCount >= 184 {
+                currentFrameCount %= 183
+            }
             if let line = self.line {
                 line.removeFromParent()
             }
-            shortPointList = shortPointList.map({ (point) -> CGPoint in
-                return CGPoint(x: point.x + 1, y: point.y + 1)
-            })
-            self.line = SKShapeNode(points: &shortPointList, count: shortPointList.count)
-            self.line?.lineWidth = 100
+//            shortPointList = shortPointList.map({ (point) -> CGPoint in
+//                return CGPoint(x: point.x + 1, y: point.y + 1)
+//            })
+            let cgPointList: [[CGPoint]] = self.realStageList.map({return $0.img.map({return CGPoint(x: CGFloat($0["x"] as! Int) * CGFloat(3.38) - CGFloat(self.size.width / 2), y: self.size.height - CGFloat($0["y"] as! Int) * CGFloat(3.38) - CGFloat(self.size.height / 2))})})
+            
+            var tempCgList: [CGPoint] = cgPointList[(cgPointList.count - currentFrameCount) % cgPointList.count]
+            self.line = SKShapeNode(points: &tempCgList, count: tempCgList.count)
+            let path = CGMutablePath()
+            path.addLines(between: tempCgList)
+            path.closeSubpath()
+            self.line?.physicsBody = SKPhysicsBody(edgeChainFrom: path)
+            self.line?.physicsBody?.categoryBitMask = self.groundCategory
+            self.line?.physicsBody?.contactTestBitMask = self.bikeManCategory
+            self.line?.physicsBody?.collisionBitMask = self.bikeManCategory
+            self.line?.physicsBody?.affectedByGravity = false
+            self.line?.physicsBody?.isDynamic = false
+            self.line?.physicsBody?.pinned = true
+            self.line?.lineWidth = 10
             self.line?.zPosition = 1
             self.line?.strokeColor = UIColor.red
             self.addChild(self.line!)
@@ -108,12 +173,11 @@ final class GameScene: SKScene {
 //                        }
                         
                         return CGPoint(x: CGFloat(Int(arr[3]) ?? 0) * magnification - size.width / 2, y: CGFloat((Int(arr[4]) ?? 0)) * magnification - size.height / 2)
-                        
                     }
                 }
                 pointList = Array(pointList[1..<pointList.count-1])
-                print(pointList)
-                print(pointList.count)
+//                print(pointList)
+//                print(pointList.count)
                 
             } catch let error as NSError {
                 print(error.localizedDescription)
@@ -123,6 +187,8 @@ final class GameScene: SKScene {
     
     private func setupNodesInGameScene() {
         bikeMan = childNode(withName: "bikeMan") as? SKSpriteNode
+        bikeMan?.physicsBody?.categoryBitMask = bikeManCategory
+        bikeMan?.physicsBody?.contactTestBitMask = groundCategory
         bikeMan?.zPosition = 1
         defaultGround = childNode(withName: "defaultGround") as? SKSpriteNode
         defaultGround?.zPosition = 1
@@ -131,13 +197,16 @@ final class GameScene: SKScene {
     }
 
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if !isGameStarted {
-            isGameStarted = true
-            isAnimating = true
-            startGame()
-            return
-        }
-        
+//        if let countdownLabel = self.countdownLabel {
+//            countdownLabel.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
+//        }
+//        if !isGameStarted {
+//            isGameStarted = true
+//            isAnimating = true
+//            startGame()
+//            return
+//        }
+//
         let touch = touches.first
         if let location = touch?.location(in: self) {
             let theNodes = nodes(at: location)
@@ -148,32 +217,88 @@ final class GameScene: SKScene {
                 }
             }
         }
-        
         jump()
     }
     
-    private func createMovie() {
+    func countdown(count: Int) {
+        countdownLabel = childNode(withName: "countdownLabel") as? SKLabelNode
+        if let countdownLabel = countdownLabel {
+            countdownLabel.horizontalAlignmentMode = .center
+            countdownLabel.verticalAlignmentMode = .baseline
+//            countdownLabel.position = CGPoint(x: , y: size.height*(1/3))
+            countdownLabel.fontColor = .white
+//            countdownLabel.fontSize = size.height / 30
+            countdownLabel.zPosition = 100
+            countdownLabel.text = "\(count)"
+        }
+        countdownLabel?.isHidden = false
+        countdownLabel?.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
+ 
+        let counterDecrement = SKAction.sequence([SKAction.wait(forDuration: 1),
+                                                  SKAction.run(countdownAction)])
+        
+        run(SKAction.sequence([SKAction.repeat(counterDecrement, count: 3),
+                                     SKAction.run(endCountdown)]))
+    }
+    
+    func countdownPulse() {
+        countdownLabel?.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
+    }
+    
+    func countdownAction() {
+        count -= 1
+        countdownLabel?.text = "\(count)"
+        countdownLabel?.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
+    }
+
+    func endCountdown() {
+        countdownLabel?.removeFromParent()
+        if !isGameStarted {
+            isGameStarted = true
+            isAnimating = true
+            startGame()
+            return
+        }
+    }
+    
+    private func createMovieBG() {
+        movieBG = SKSpriteNode(imageNamed: "image_1")
+        if let movieBG = movieBG {
+            movieBG.position = CGPoint(x: 360 / 2 * 3.38 - size.width / 2, y: 720 / 2 * 3.38 - size.height / 2)
+            movieBG.size.height *= 3.38
+            movieBG.size.width  *= 3.38
+            addChild(movieBG)
+        }
+    }
+    
+    private func animateMovie() {
         var frames = [SKTexture]()
         
-        for i in 1...60 {
-            let frame = SKTexture.init(imageNamed: "image_\(i)")
+        for i in 1...183 {
+            let index = 184 - i
+            let frame = SKTexture.init(imageNamed: "image_\(index)")
             frames.append(frame)
         }
-        
-        let node = SKSpriteNode(imageNamed: "image_1")
-        node.position = CGPoint(x: 0, y: 0)
-        node.size.height = size.height
-        node.size.width  = size.width
-        let animation = SKAction.animate(with: frames, timePerFrame: 0.03)
-//        SKAction.animte
-        node.run(SKAction.repeatForever(animation))
-        
-        addChild(node)
+        if let movieBG = movieBG {
+            movieBG.removeFromParent()
+            movieBG.position = CGPoint(x: 360 / 2 * 3.38  - size.width / 2, y: 720 / 2 * 3.38 - size.height / 2)
+//            movieBG.size.height *= 3.38
+//            movieBG.size.width  *= 3.38
+//            let originalHeight: CGFloat = 720
+//            let newHeight = frame.size.height
+//            movieBG.size.height = size.height
+//            magnification = newHeight / originalHeight
+//            movieBG.size.width  = size.width * magnification
+            let animation = SKAction.animate(with: frames, timePerFrame: 0.03)
+            movieBG.run(SKAction.repeatForever(animation))
+            addChild(movieBG)
+        }
     }
     
     private func startAnimation() {
-        let moveDefaultGround = SKAction.moveBy(x: -defaultGround!.size.width, y: 0, duration: TimeInterval(defaultGround!.size.width) / scrollSpeed)
-        defaultGround?.run(moveDefaultGround)
+//        let moveDefaultGround = SKAction.moveBy(x: -defaultGround!.size.width, y: 0, duration: TimeInterval(defaultGround!.size.width) / scrollSpeed)
+//        defaultGround?.run(moveDefaultGround)
+        defaultGround?.removeFromParent()
         
         let moveLine = SKAction.moveBy(x: -1200 * magnification + size.width, y: 0, duration: TimeInterval(1200 * magnification) / scrollSpeed)
         line?.run(moveLine)
@@ -182,6 +307,8 @@ final class GameScene: SKScene {
             let moveBackground = SKAction.moveBy(x: -background.size.width + size.width, y: 0, duration: TimeInterval(background.size.width) / scrollSpeed)
             background.run(moveBackground)
         }
+        createRealTimeLine()
+        animateMovie()
     }
     
     private func resetGame() {
@@ -345,6 +472,19 @@ final class GameScene: SKScene {
                 isAnimating = false
                 gameClear()
             }
+        }
+    }
+}
+
+
+extension GameScene: SKPhysicsContactDelegate {
+    func didBegin(_ contact: SKPhysicsContact) {
+        if contact.bodyA.categoryBitMask == bikeManCategory {
+            print("contact!")
+        }
+        
+        if contact.bodyB.categoryBitMask == bikeManCategory {
+            print("contact!")
         }
     }
 }
